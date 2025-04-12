c6fc692eaf1a08101dbe4592d2aec6de1ba19846
phawitb <96680592+phawitb@users.noreply.github.com>
2025-04-12 21:16:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -173,6 +173,6 @@ def confirm_order():
     })
 
 
-if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5007, debug=True) #,debug=False)
+# if __name__ == '__main__':
+#     # app.run(debug=True)
+#     app.run(host='0.0.0.0', port=5007, debug=True) #,debug=False)
